f6b9a5f8e65d372365ec965b7f99fe5b907bd9d1
jack <jack008007z@outlook.es>
2024-06-01 04:43:05 -0500

    feat(implement views sales)

---
+++ app/src/main/java/com/mibodega/mystore/shared/adapters/RecyclerViewAdapterProductSearch.java
@@ -0,0 +1,4 @@
+package com.mibodega.mystore.shared.adapters;
+
+public class RecyclerViewAdapterProductSearch {
+}
---
+++ app/src/main/java/com/mibodega/mystore/views/sales/CreateSaleFragment.java
@@ -0,0 +1,23 @@
+package com.mibodega.mystore.views.sales;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mibodega.mystore.R;
+
+public class CreateSaleFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_sale, container, false);
+    }
+}
